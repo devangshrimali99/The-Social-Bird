064c9d7ecbae2a2cedfaf550e9953b0a2ccc6b8e
Devang Shrimali <76675646+devangshrimali99@users.noreply.github.com>
2023-03-07 14:25:43 +0530

    Update FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -11,7 +11,7 @@ const FriendListWidget = ({ userId, ownerId }) => {
   const friends = useSelector((state) => state.user.friends);
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://the-social-bird.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
